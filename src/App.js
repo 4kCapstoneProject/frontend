6db9c1e26b17461d6e8848d82c782726742b82ca
cloud0406 <cloud0406@naver.com>
2022-04-08 20:44:04 +0900

    * 로그인 & 계정 생성 UI 구현 및 라우터
    * 로그인 화면 ui
    * 계정 생성 ui
    * 각 페이지 컴포넌트들 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // 라우터 사용위해 import -> 버전 바뀐거 확인해서 그에 맞게 사용
 import Home from "./routes/Home";
 import Login from "./routes/Login";
+import Register from "./routes/Register";
 import Streaming from "./routes/Streaming";
 import Targetinfo from "./routes/Targetinfo";
 import Upload from "./routes/Upload";
@@ -10,6 +11,9 @@ function App() {
       <Switch>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
         <Route path="/streaming">
           <Streaming />
