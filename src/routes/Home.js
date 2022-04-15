2a7bea2a34fd9e9a975cdeb09a4fccd79cdc7151
cloud0406 <cloud0406@naver.com>
2022-04-15 20:32:05 +0900

    test
    * test version

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,35 @@
+import React, { useState } from "react";
+import { connect } from "react-redux"; // connect함수 쓰기위해 import
+import { userAdd } from "../redux/user";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { getCookie, removeCookie, setCookie } from "../shared/cookie";
+import Cookies from "universal-cookie";
+import Login from "./Login";
+
 function Home() {
-  return <h1>Hello</h1>;
+  const [isLogin, setIsLogin] = useState(true);
+  const onClick = (event) => {
+    event.preventDefault();
+    removeCookie("loginCookie");
+    setIsLogin(false);
+  };
+  const cookieTest = (event) => {
+    event.preventDefault();
+    console.log(getCookie("loginCookie"));
+  };
+  return (
+    <>
+      {isLogin ? (
+        <div>
+          <button onClick={onClick}>로그 아웃</button>
+          <button onClick={cookieTest}>쿠키 확인</button>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </>
+  );
 }
 
 export default Home;
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -5,10 +5,14 @@ import { emailCheck } from "../shared/common";
 import axios from "axios";
 import "./LoginRegister.css";
 import { Link } from "react-router-dom";
+import { getCookie, setCookie } from "../shared/cookie";
+import Cookies from "universal-cookie";
+import Home from "./Home";
 
 function Login({ userAdd }) {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
   const onEmailHandler = (event) => {
     setId(event.currentTarget.value);
@@ -19,7 +23,7 @@ function Login({ userAdd }) {
   };
 
   const onClick = (event) => {
-    // event.preventDefault();
+    event.preventDefault();
     if (id === "" || password === "") {
       window.alert("아이디와 비밀번호를 입력해주세요.");
       return;
@@ -45,57 +49,73 @@ function Login({ userAdd }) {
           res.data.auth,
           res.data.email
         );
-        const accessToken = res.data.token;
 
         window.alert("로그인 성공!!");
         //쿠키에 토큰 저장
         //   setCookie("is_login", ${accessToken});
         //   document.location.href = "/";
+        setCookie("loginCookie", res.data.token, {
+          path: "/",
+          expires: Math.floor(Date.now() / 1000) + 60 * 60,
+        });
+        console.log(getCookie("loginCookie"));
+        setIsLogin(true);
       })
       .catch((error) => {
         window.alert(error);
         console.log(error);
       });
+
+    setId("");
+    setPassword("");
   };
 
   return (
-    <div className="loginregister">
-      <form>
-        <h2>로그인</h2>
-        <div className="input-box">
-          <input
-            name="id"
-            type="id"
-            placeholder="이메일"
-            value={id}
-            onChange={onEmailHandler}
-          />
-          <label htmlFor="username">아이디</label>
-        </div>
-        <div className="input-box">
-          <input
-            name="password"
-            type="password"
-            placeholder="비밀번호"
-            value={password}
-            onChange={onPasswordHandler}
-          />
-          <label htmlFor="password">비밀번호</label>
-        </div>
+    <>
+      {isLogin ? (
+        <Home />
+      ) : (
+        <div className="loginregister">
+          <form>
+            <h2>로그인</h2>
+            <div className="input-box">
+              <input
+                name="id"
+                type="text"
+                placeholder="이메일"
+                value={id}
+                onChange={onEmailHandler}
+              />
+              <label htmlFor="username">아이디</label>
+            </div>
+            <div className="input-box">
+              <input
+                name="password"
+                type="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={onPasswordHandler}
+              />
+              <label htmlFor="password">비밀번호</label>
+            </div>
 
-        <div>
-          {/* 버튼? or input? */}
-          {/* <button type="submit" onSubmit={onSubmit}> */}
-          <button onClick={onClick}>로그인</button>
+            <div>
+              {/* 버튼? or input? */}
+              {/* <button type="submit" onSubmit={onSubmit}> */}
+              <button className="create_login" onClick={onClick}>
+                로그인
+              </button>
+            </div>
+            <div id="not_register">
+              아직 회원이 아니신가요?{" "}
+              <Link to="/register">
+                <button id="create_id">회원 가입</button>
+              </Link>
+            </div>
+          </form>
         </div>
-        <div id="not_register">
-          아직 회원이 아니신가요?{" "}
-          <Link to="/register">
-            <button id="create_id">회원 가입</button>
-          </Link>
-        </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 }
 
