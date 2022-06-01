81afcf2e99bafbe1655e971188c8304947ed21c4
cloud0406 <cloud0406@naver.com>
2022-06-01 18:52:56 +0900

    test
    test

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -697,8 +697,8 @@ function Home({ users, addPk, userAdd }) {
                                   <Button
                                     size="small"
                                     color="primary"
-                                    // value={index}
-                                    value={imgItem.targetPk}
+                                    value={index}
+                                    // value={imgItem.targetPk}
                                     // onClick={onStreaming}
                                     onClick={saveUserInfo}
                                   >
